19404772f13955235adbd3ed61bf7ad4bbb91ff3
Suraj Deshmukh <surajd.service@gmail.com>
2018-06-04 15:01:34 +0530

    remove the name rabbitmq

--- test2/build-deploy.sh
+++ test2/build-deploy.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 go build -o sender/sender sender/sender.go
-docker build -t docker.io/surajd/rabbitmq-sender:1.0.1 ./sender
+docker build -t docker.io/surajd/activemq-sender:1.0.1 ./sender
 
 go build -o receiver/receiver receiver/receiver.go
-docker build -t docker.io/surajd/rabbitmq-receiver:1.0.1 ./receiver
+docker build -t docker.io/surajd/activemq-receiver:1.0.1 ./receiver
 
 oc new-project amqptest2
 kedge apply -f kedge.yml
--- test3/build-deploy.sh
+++ test3/build-deploy.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 go build -o sender/sender sender/sender.go
-docker build -t docker.io/surajd/rabbitmq-sender:1.0.1 ./sender
+docker build -t docker.io/surajd/activemq-sender:1.0.1 ./sender
 
 go build -o receiver1/receiver receiver1/receiver.go
-docker build -t docker.io/surajd/rabbitmq-receiver1:1.0.1 ./receiver1
+docker build -t docker.io/surajd/activemq-receiver1:1.0.1 ./receiver1
 
 go build -o receiver2/receiver receiver2/receiver.go
-docker build -t docker.io/surajd/rabbitmq-receiver2:1.0.1 ./receiver2
+docker build -t docker.io/surajd/activemq-receiver2:1.0.1 ./receiver2
 
 oc new-project amqptest3
 kedge apply -f kedge.yml
