af9ec4e540a41d62f4a2ad7493152d90f655caab
Suraj Deshmukh <surajd.service@gmail.com>
2018-06-06 18:23:52 +0530

    no need of redundant receiver2

--- test3/build-deploy.sh
+++ test3/build-deploy.sh
@@ -5,11 +5,8 @@ eval $(minishift docker-env)
 go build -o sender/sender sender/sender.go
 docker build -t docker.io/surajd/activemq-sender:1.0.1 ./sender
 
-go build -o receiver1/receiver receiver1/receiver.go
-docker build -t docker.io/surajd/activemq-receiver1:1.0.1 ./receiver1
-
-go build -o receiver2/receiver receiver2/receiver.go
-docker build -t docker.io/surajd/activemq-receiver2:1.0.1 ./receiver2
+go build -o receiver/receiver receiver/receiver.go
+docker build -t docker.io/surajd/activemq-receiver:1.0.1 ./receiver
 
 oc new-project amqptest3
 kedge apply -f kedge.yml
