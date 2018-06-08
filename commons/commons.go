17045bd1e4910bf3d5acae70eac60522b84e4338
Suraj Deshmukh <surajd.service@gmail.com>
2018-06-08 15:23:50 +0530

    refactor commons

--- commons/commons.go
+++ commons/commons.go
@@ -15,15 +15,42 @@ func getCreds() (string, string) {
 	return username, password
 }
 
+func getInsecureSkipVerify() bool {
+	insecureSkipVerify := os.Getenv("INSECURE_SKIP_VERIFY")
+	if insecureSkipVerify == "true" {
+		return true
+	}
+	return false
+}
+
+func isAnonAuth() bool {
+	isAnon := os.Getenv("ANONYMOUS_AUTH")
+	if isAnon == "true" {
+		return true
+	}
+	return false
+}
+
 func GetAMQPSession() (*amqp.Session, func()) {
 
 	username, password := getCreds()
+	var options []amqp.ConnOption
+	if isAnonAuth() {
+		options = append(options, amqp.ConnSASLAnonymous())
+	} else {
+		options = append(options, amqp.ConnSASLPlain(username, password))
+	}
+
+	if getInsecureSkipVerify() {
+		options = append(options, amqp.ConnTLSConfig(&tls.Config{
+			InsecureSkipVerify: true,
+		}))
+	}
+
+	log.Println("[*] dialing AMQP server")
 	client, err := amqp.Dial(
 		os.Getenv("AMQ_SERVER"),
-		amqp.ConnSASLPlain(username, password),
-		amqp.ConnTLSConfig(&tls.Config{
-			InsecureSkipVerify: true,
-		}),
+		options...,
 	)
 	if err != nil {
 		log.Fatal("[!] dialing AMQP server:", err)
