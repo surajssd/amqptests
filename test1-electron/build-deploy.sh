3f37ae2181652eb1dadbad789fb4a055f40e650d
Suraj Deshmukh <surajd.service@gmail.com>
2018-06-12 11:11:18 +0530

    using electron for test1

---
+++ test1-electron/build-deploy.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -e
+eval $(minishift docker-env)
+set +e
+
+go build -o sender/sender sender/sender.go
+docker build -t docker.io/surajd/activemq-sender:fed-1.0.1 ./sender
+
+go build -o receiver/receiver receiver/receiver.go
+docker build -t docker.io/surajd/activemq-receiver:fed-1.0.1 ./receiver
+
+oc new-project amqptest-electron1
+kedge apply -f kedge.yml
+
+oc get pods
---
+++ test1-electron/sender/sender.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"strings"
+	"time"
+
+	"qpid.apache.org/amqp"
+	"qpid.apache.org/electron"
+)
+
+func main() {
+	container := electron.NewContainer(os.Getenv("POD_NAME"))
+
+	url, err := amqp.ParseURL(os.Getenv("AMQ_SERVER"))
+	if err != nil {
+		log.Fatal("[!] parsing amqp url:", err)
+	}
+
+	c, err := container.Dial("tcp", url.Host)
+	if err != nil {
+		log.Fatal("[!] dialing the amqp server:", err)
+	}
+	addr := strings.TrimPrefix(url.Path, "/")
+	s, err := c.Sender(electron.Target(addr))
+	if err != nil {
+		log.Fatal("[!] creating sender:", err)
+	}
+
+	count := 0
+	// endless for loop which keeps sending data
+	for {
+		m := amqp.NewMessage()
+		msg := fmt.Sprintf("hello from %q on %q! %d", os.Getenv("TYPE_OF_AMQP_USER"), os.Getenv("POD_NAME"), count)
+		m.Marshal(msg)
+		outcome := s.SendSync(m)
+		if outcome.Error != nil {
+			log.Print("[!] sending message:", outcome.Value, ", error:", outcome.Error)
+			// continue to retry sending the message
+			continue
+		} else if outcome.Status != electron.Accepted {
+			log.Print("[!] sending message:", outcome.Value, ", unexpected status:", outcome.Status)
+			// continue to retry sending the message
+			continue
+		}
+
+		log.Println("[*] sent message:", msg)
+		count++
+		time.Sleep(2 * time.Second)
+	}
+
+}
